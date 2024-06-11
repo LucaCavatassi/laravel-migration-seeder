e9c7896d9347695fd85ad070b061430e168d0860
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-06-11 17:01:35 +0200

    feat: Table display on views of data fetched from db

--- resources/views/trains.blade.php
+++ resources/views/trains.blade.php
@@ -1,7 +1,54 @@
 @extends('layouts.app')
 
+
 @section('content')
-    @foreach ($trains as $train)
-        <h1>{{ $train->company }}</h1>
-    @endforeach
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Company</th>
+                <th scope="col">Departure</th>
+                <th scope="col">Arrival</th>
+                <th scope="col">Time Departure</th>
+                <th scope="col">Time Arrival</th>
+                <th scope="col">Train Number</th>
+                <th scope="col">In Time</th>
+                <th scope="col">Cancelled</th>
+                <th scope="col">Date</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            @foreach ($trains as $train)
+            <tr>
+                <th scope="row">{{ $train->id }}</th>
+                <td>{{ $train->company }}</td>
+                <td>{{ $train->departure }}</td>
+                <td>{{ $train->arrival }}</td>
+                <td>{{ $train->time_departure }}</td>
+                <td>{{ $train->time_arrival }}</td>
+                <td>{{ $train->code }}</td>
+
+                @if ($train->in_time === 0)
+                    <td>No</td>
+                @else
+                    <td>Si</td>
+                @endif
+
+                @if ($train->cancelled === 0)
+                    <td>No</td>
+                @else
+                    <td>Si</td>
+                @endif
+
+
+                @if ($train->date === date("Y-m-d"))
+                <td> Oggi </td>
+                @else
+                <td> {{ $train->date }} </td>
+                @endif
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
 @endsection
