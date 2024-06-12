1676786033d6f60c4032cf21289618c342f32bf6
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-06-12 13:14:27 +0200

    feat: created 15 faker records

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -14,20 +14,21 @@ class TrainsTableSeeder extends Seeder {
     public function run(): void {
         $faker = Faker::create('it_IT');
 
-
-        $newTrain = new Train(); // Istanza del model
-        $newTrain->company = $faker->randomElement(['Italo','Trenitalia']);
-        $newTrain->departure = $faker->city();
-        $newTrain->arrival = $faker->city();
-        $newTrain->time_departure = $faker->time();
-        $newTrain->time_arrival = $faker->time();
-        $newTrain->code = $faker->asciify('*****');
-        $newTrain->cabin_numbers = $faker->randomDigitNotNull();
-        $newTrain->in_time = $faker->boolean();
-        $newTrain->cancelled = $faker->boolean();
-        $newTrain->date = $faker->dateTimeInInterval('-1 week', '+3 days');
-        // dd($newTrain); // Stamperà un model di house
-        
-        $newTrain->save(); // Per salvare i dati e aggiungerli al database
+        for ($i = 0; $i < 15; $i++) {
+            $newTrain = new Train(); // Istanza del model
+            $newTrain->company = $faker->randomElement(['Italo','Trenitalia']);
+            $newTrain->departure = $faker->city();
+            $newTrain->arrival = $faker->city();
+            $newTrain->time_departure = $faker->time();
+            $newTrain->time_arrival = $faker->time();
+            $newTrain->code = $faker->asciify('*****');
+            $newTrain->cabin_numbers = $faker->randomDigitNotNull();
+            $newTrain->in_time = $faker->boolean();
+            $newTrain->cancelled = $faker->boolean();
+            $newTrain->date = $faker->dateTimeInInterval('-1 day', '+5 days');
+            // dd($newTrain); // Stamperà un model di house
+            
+            $newTrain->save(); // Per salvare i dati e aggiungerli al database
+        }
     }
 }
