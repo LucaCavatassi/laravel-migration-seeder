0279a646cd0fec56c643ec0e72057f1a782f5137
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-06-12 13:09:42 +0200

    feat: first fake record pushed in db

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -4,11 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Train;
+use Illuminate\Support\Carbon;
 
 class TrainController extends Controller
 {
     public function filtered() {
-        $trains = Train::where("date", "2024-06-11")->get();
+        $trains = Train::where("date", Carbon::today())->get();
         return view("today-trains", compact("trains"));
     }
 
---
+++ database/seeders/TrainsTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Train;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+
+class TrainsTableSeeder extends Seeder {
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void {
+        $faker = Faker::create('it_IT');
+
+
+        $newTrain = new Train(); // Istanza del model
+        $newTrain->company = $faker->randomElement(['Italo','Trenitalia']);
+        $newTrain->departure = $faker->city();
+        $newTrain->arrival = $faker->city();
+        $newTrain->time_departure = $faker->time();
+        $newTrain->time_arrival = $faker->time();
+        $newTrain->code = $faker->asciify('*****');
+        $newTrain->cabin_numbers = $faker->randomDigitNotNull();
+        $newTrain->in_time = $faker->boolean();
+        $newTrain->cancelled = $faker->boolean();
+        $newTrain->date = $faker->date('Y_m_d');
+        // dd($newTrain); // Stamperà un model di house
+        
+        $newTrain->save(); // Per salvare i dati e aggiungerli al database
+    }
+}
